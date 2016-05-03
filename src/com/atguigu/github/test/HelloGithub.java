58b04586bde364e74c875365416424ed207b08db
EagleRisen <2602154706@qq.com>
2016-05-03 08:23:12 +0800

    2

--- src/com/atguigu/github/test/HelloGithub.java
+++ src/com/atguigu/github/test/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("你好 ！GitHub01");
 		System.out.println("收到！ Client");
+		System.out.println("你好 ！GitHub02");
 	}
 
 }
